18bc78d193f587e8773789448ca54f179b4e920e
Yurino Oga <yurino.o@latona.io>
2021-11-30 17:04:01 +0900

    release

--- SAP_API_Caller/responses/material_stock.go
+++ SAP_API_Caller/responses/material_stock.go
@@ -1,6 +1,6 @@
 package responses
 
-type MRPArea struct {
+type MaterialStock struct {
 	D struct {
 		Results []struct {
 			Metadata struct {
@@ -8,28 +8,11 @@ type MRPArea struct {
 				URI  string `json:"uri"`
 				Type string `json:"type"`
 			} `json:"__metadata"`
-			Product                       string  `json:"Product"`
-			Plant                         string  `json:"Plant"`
-			MRPArea                       string  `json:"MRPArea"`
-			MRPType                       string  `json:"MRPType"`
-			MRPResponsible                string  `json:"MRPResponsible"`
-			MRPGroup                      string  `json:"MRPGroup"`
-			ReorderThresholdQuantity      string  `json:"ReorderThresholdQuantity"`
-			PlanningTimeFence             int     `json:"PlanningTimeFence"`
-			LotSizingProcedure            string  `json:"LotSizingProcedure"`
-			LotSizeRoundingQuantity       float64 `json:"LotSizeRoundingQuantity"`
-			MinimumLotSizeQuantity        float64 `json:"MinimumLotSizeQuantity"`
-			MaximumLotSizeQuantity        float64 `json:"MaximumLotSizeQuantity"`
-			MaximumStockQuantity          float64 `json:"MaximumStockQuantity"`
-			ProcurementSubType            string  `json:"ProcurementSubType"`
-			DfltStorageLocationExtProcmt  string  `json:"DfltStorageLocationExtProcmt"`
-			MRPPlanningCalendar           string  `json:"MRPPlanningCalendar"`
-			SafetyStockQuantity           string  `json:"SafetyStockQuantity"`
-			SafetyDuration                string  `json:"SafetyDuration"`
-			FixedLotSizeQuantity          string  `json:"FixedLotSizeQuantity"`
-			PlannedDeliveryDurationInDays int     `json:"PlannedDeliveryDurationInDays"`
-			StorageLocation               string  `json:"StorageLocation"`
-			IsMarkedForDeletion           string  `json:"IsMarkedForDeletion"`
+		Material        string   `json:"Material"`
+		Plant           string   `json:"Plant"`
+		StorageLocation string   `json:"StorageLocation"`
+		Batch           string   `json:"Batch"`
+		ToMaterialStock string   `json:"to_MaterialStock"`
 		} `json:"results"`
 	} `json:"d"`
 }
